eea5a9c190753fb5132a09559913ba67e5e15b91
Yuri Mirgorod <yuri.mirgorod@gmail.com>
2025-11-28 16:44:41 +0300

    Moved user roles from UserManagement to Contracts

---
+++ Innoshop.Contracts/UserManagement/UserRoles/Role.cs
@@ -0,0 +1,15 @@
+namespace Innoshop.Contracts.UserManagement.UserRoles;
+
+public class Role : Enumeration<Role>
+{
+    // Newly added static Roles must me migrated to DB
+    public static readonly Role Administrator = new(1, "Administrator");
+    public static readonly Role Customer = new(2, "Customer");
+
+    public Role(int id, string name)
+        : base(id, name)
+    {
+    }
+    
+    public static implicit operator string(Role role) => role.Name;
+}
